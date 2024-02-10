8be95727412dfd5ace19d7022ac53258ad2622fa
majadev <mgedda@live.se>
2024-02-10 15:19:08 +0100

    fun stuff

--- modules/photo.js
+++ modules/photo.js
@@ -2,6 +2,8 @@ const renderUI = () => {
     const photo = JSON.parse(localStorage.getItem('clickedPhoto'));
     document.querySelector('.site-heading').innerText = photo.user.name;
     document.querySelector('.photo-container').style.backgroundImage = `url('${photo.urls.regular}')`;
+    document.querySelector('.photo-info').innerHTML = photo.description;
+
 }
 
 renderUI();
